e8ef4fb9d6397c29042cb259dccbfdac4d878034
Michael Vogel <voguesie@alumni.stanford.edu>
2019-12-05 03:17:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
+
+// use .push to add name to end of kittens
 function destructivelyAppendKitten(name) {
   var newKittens = kittens.push(name);
   return newKittens
